1fc5b4a0e2ceae6a7a5b504e76a7b29974493a8c
Minsung <alscks6521@naver.com>
2025-01-09 13:08:11 +0900

    Update web build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2136eaef1536a7934a238e54694e5574",
+const RESOURCES = {"flutter_bootstrap.js": "be52a3be515319cbd3aeb1cf65b57645",
 "version.json": "58229f82b089f18fe9cc5cc2a4052fd7",
 "index.html": "28bf314535a3649f6cde50d77d5cef61",
 "/": "28bf314535a3649f6cde50d77d5cef61",
-"main.dart.js": "a6e34d125a6c42300a2653a70e278bb6",
+"main.dart.js": "0cad2419c8f549f91a825db0b9408c14",
 "404.html": "21a3005f3f80e778be7ace75fd6a75bf",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
